ffe20ebcd12f70a8ac9f5cae772480059ee6660b
kbwhodat <kato.byantalo@aiazing.com>
2024-07-07 07:33:46 -0400

    upddate

--- scripts/setcookies.py
+++ scripts/setcookies.py
@@ -1,24 +1,29 @@
-import undetected_chromedriver as uc
+from selenium_profiles.webdriver import Chrome
+from selenium_profiles.profiles import profiles
+from seleniumwire import webdriver
 import os
 import json
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 def gen_driver():
     try:
-        chrome_options = uc.ChromeOptions()
+        profile = profiles.Windows()
+        chrome_options = webdriver.ChromeOptions()
         user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-        chrome_options.add_argument('--headless')
+
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument(f"--user-agent={user_agent}")
+        chrome_options.add_argument("--headless=new")
         chrome_options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.popups": 1,  # Allow pop-ups
+            "profile.default_content_setting_values.cookies": 1, 
+            "profile.cookie_controls_mode": 0,
+            "profile.block_third_party_cookies": False,
         })
 
 
-        driver = uc.Chrome(options=chrome_options, driver_executable_path='bin/chromedriver')
+        driver = Chrome(profile, options=chrome_options, uc_driver=False)
         return driver
     except Exception as e:
         print("Error in Driver: ",e)
@@ -26,19 +31,24 @@ def gen_driver():
 def main():
     driver = gen_driver()  # Initialize the driver using your function
     try:
-        time.sleep(1)
+        time.sleep(2)
         location = "scripts/cookies.json"
 
+        driver.get('https://youtube.com')
+        time.sleep(2)
         driver.execute_script("window.open('https://en.rarbg-official.com/episodes/true-detective-2014-season-4-episode-1','_blank');")
+
 
         time.sleep(2)
         driver.switch_to.window(driver.window_handles[1])
 
-        WebDriverWait(driver, 10).until(EC.url_to_be('https://en.rarbg-official.com/episodes/true-detective-2014-season-4-episode-1'))
-        time.sleep(2)
+        time.sleep(5)
 
+        cookies = driver.get_cookie("cf_clearance")
+        cookie_list = [cookies] if cookies else []
         with open(location, 'w') as filehandler:
-            json.dump(driver.get_cookies(), filehandler, indent=4)
+            json.dump(cookie_list, filehandler, indent=4)
+
             
     except Exception as e:
         print("Error in Driver: ",e)
