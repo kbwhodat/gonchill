30b9a4dd7718ee1e2ea7b8ac91ab60ecad4898ac
kbwhodat <byantalok@yahoo.com>
2024-07-13 19:19:15 -0400

    updating overlay nix

--- overlay.nix
+++ overlay.nix
@@ -9,9 +9,17 @@ self: super:
     src = super.fetchFromGitHub {
       owner = "kbwhodat";
       repo = "gonchill";
-      rev = "${version}";
-      sha256 = lib.fakesha256;
+      rev = "v${version}";
+      hash = "sha256-Gjw1dRrgM8D3G7v6WIM2+50r4HmTXvx0Xxme2fH9TlQ=";
     };
 
+    vendorHash = null;
+
+    meta = {
+      description = "What whatever you want...";
+      homepage = "https://github.com/kbwhodat/gonchill";
+      license = lib.licenses.mit;
+      maintainers = with lib.maintainers; [ kbwhodat ];
+    };
   };
 }
