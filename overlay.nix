51719093ac6dd98e31bc82a2f42a60e05d2936a3
kbwhodat <byantalok@yahoo.com>
2024-07-13 19:39:21 -0400

    switch up overlay approach

--- overlay.nix
+++ overlay.nix
@@ -1,19 +1,31 @@
 { lib, ...}:
 self: super:
 {
-  gonchill = super.buildGoModule rec {
+  gonchill = super.stdenv.mkDerivation rec {
     pname = "gonchill";
     version = "1.0.6";
 
     src = ./.;
 
-    vendorSha256 = "f11c215ec98a4665603b28b6c064948cf42435b48fe5386020046b75ca242e8b";
+    buildInputs = [ super.go ];
+    sourceRoot = ".";
+    phases = [ "unpackPhase" "installPhase" ];
 
-    meta = {
-      description = "What whatever you want...";
+    installPhase = ''
+      runHook preInstall
+
+      go build -o gonchill
+      mkdir -p $out/bin
+      cp gonchill $out/bin/
+
+      runHook postInstall
+    '';
+
+    meta = with lib.super.lib; {
+      description = "Watch whatever you want...";
       homepage = "https://github.com/kbwhodat/gonchill";
       license = lib.licenses.mit;
-      maintainers = with lib.maintainers; [ kbwhodat ];
+      maintainers = [ lib.maintainers.kbwhodat ];
     };
   };
 }
