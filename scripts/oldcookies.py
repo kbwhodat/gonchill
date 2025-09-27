0f0820343a47550556fabced6c639ec427fd8aa5
kbwhodat <byantalok@yahoo.com>
2025-09-27 19:54:42 -0400

    fix: update userAgent to 140

--- scripts/oldcookies.py
+++ scripts/oldcookies.py
@@ -15,7 +15,7 @@ def gen_driver():
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument("--window-size=1920,1080")
         chrome_options.add_argument(f"--user-agent={user_agent}")
-        chrome_options.add_argument("--headless=new")
+        # chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_experimental_option("prefs", {
--- scripts/setcookies.py
+++ scripts/setcookies.py
@@ -3,6 +3,8 @@ from selenium_driverless.types.by import By
 import json
 import asyncio
 
+# note to author: if I forget why this fails make sure to double check the series.go and movies.go files userAgent variable
+
 async def main():
 
     chrome_options = webdriver.ChromeOptions()
@@ -11,8 +13,9 @@ async def main():
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-features=DisableLoadExtensionCommandLineSwitch")
     chrome_options.add_experimental_option("prefs", {
-        "profile.default_content_setting_values.popups": 1,  # Allow pop-ups
+        "profile.default_content_setting_values.popups": 0,  # Allow pop-ups
         "profile.default_content_setting_values.cookies": 1,
         "profile.cookie_controls_mode": 0,
         "profile.block_third_party_cookies": False,
@@ -23,12 +26,13 @@ async def main():
         await driver.get('https://en.rarbg-official.com/episodes/true-detective-2014-season-4-episode-1', wait_load=True)
         await driver.sleep(0.5)
         await driver.wait_for_cdp("Page.domContentEventFired", timeout=15)
-        await driver.sleep(3)
+        await driver.sleep(5)
         
         
         location = "/tmp/cookies.json"
 
         cookies = await driver.get_cookie("cf_clearance")
+        print(cookies)
 
         cookie_list = [cookies] if cookies else []
         print("generating cookies...")
